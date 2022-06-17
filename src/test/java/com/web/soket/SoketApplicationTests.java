3b812277efd38e9bb15807df7835b130587a5c0b
Jun Kim <rlafbf222@naver.com>
2022-06-18 00:52:26 +0900

    웹 소켓(채팅) 샘플 코드 업로드

---
+++ src/test/java/com/web/soket/SoketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.soket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
